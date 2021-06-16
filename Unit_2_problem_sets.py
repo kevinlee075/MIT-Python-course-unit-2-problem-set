48642fcaaf6a0695bdf9cdc7e926924bb5046361
kevinlee075 <kevinlee075@gmail.com>
2021-06-16 23:49:42 +0800

    Update Unit_2_problem_sets.py

--- Unit_2_problem_sets.py
+++ Unit_2_problem_sets.py
@@ -27,7 +27,7 @@ ans = Remaining_balance_1(balance, monthlyPaymentRate, annualInterestRate)
 print('Remaining balance: ', str(ans)) #print the remaining balance
 
 
-def total_balance(balance, annualInterestRate):
+def total_balance(balance, annualInterestRate): #total balance will be added interest 12 times
     '''
     Total balance = balance * (1+aP/12)^12
     '''
@@ -48,37 +48,39 @@ def payoff_inteterest(annualInterestRate, p):
     else:
         POI = (1 + annualInterestRate/12) ** (p-1)
         return POI + payoff_inteterest(annualInterestRate, p-1)
-fixed_monthly_payment = total_balance(balance, annualInterestRate) // payoff_inteterest(annualInterestRate, 12)
-if fixed_monthly_payment < 10.0:
+fixed_monthly_payment = total_balance(balance, annualInterestRate) / payoff_inteterest(annualInterestRate, 12)
+#借錢含滾利＝還錢含滾利
+if fixed_monthly_payment < 10.0: #The requirement says the answer should be a multiple of 10.
+#If the answer is less than 10, I should print 10
     print('Lowest Payment: ' + str(10))
-else:
+else: #round to 10
     fixed_monthly_payment = round(fixed_monthly_payment, -1)
     print('Lowest Payment: ' + str(fixed_monthly_payment))
     
     
 initial_balance = int(input('balance = ')) #type the initial balance
 annualInterestRate = float(input('annualInterestRate = ')) #type the annual interest rate
-balance = initial_balance
-upperbound = balance/12.0
-lowerbound = (balance * (1 + (annualInterestRate/12) ** 12))/12.0
-payment = (upperbound + lowerbound)/2
+balance = initial_balance #define balance as the initial one
+lowerbound = balance/12.0
+upperbound = (balance * (1 + annualInterestRate/12) ** 12)/12.0
+payment = (upperbound + lowerbound)/2 #using bisection method to find the lowest payment
 def monthlyinterest():
     '''
     monthly interest = unpaid balance * monthly interest rate
     '''
     return (balance - payment) * (annualInterestRate/12)
-while balance > 0:
-    for mo in range(12):
+while balance > 0: #using looping to find the end point of balance
+    for mo in range(12): #looping 12 times (one year) to find what's the final balance
         balance = balance - payment + monthlyinterest()
-    if balance < 0:
-        upperbound = payment
-        payment = (upperbound + lowerbound)/2
+    if balance < -0.01:
+#since in many cases, there are no perfect payment numbers. Also, the requirement says
+#the lowest decimal place should be cent(0.01). Therefore, I check whether the final balance
+#could be greater than -0.01(+0.01 is no permitted since we can payoff earlier but not late).
+        balance = initial_balance #balance should be back to the initial one
+        upperbound = payment #new upperbound is used
+    elif balance > 0: #it means that the payment is still too low
         balance = initial_balance
-    elif balance > 0:
-        lowerbound = payment
-        payment = (upperbound + lowerbound)/2
-        balance = initial_balance
-    else:
-        break
-print('lowest interest: ' + str(payment))
-
+        lowerbound = payment #new lowerbound is used
+    payment = (upperbound + lowerbound)/2 #find the new bisection place
+payment = round(payment, 2) #round to 0.01
+print('lowest interest: ' + str(payment)) #print final answer
